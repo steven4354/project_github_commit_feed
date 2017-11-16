d18ce40af23abf8fe8b1c289c8e6743f4b904ea7
Lakshmi <lakshmi.nishthala@gmail.com>
2017-11-16 10:10:59 -0800

    Displayed json on browser

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
-var express = require("./express/express.js");
-var fs = require('fs')
+var express = require('./express/express.js');
+var fs = require('fs');
+let data = require('./data/commits');
+
+let dataString = JSON.stringify(data, null, 2);
+
+console.log(dataString);
 
 var app = express;
-app.listen(3000, "localhost", () => {
-  console.log('app is listening');
+app.listen(3000, 'localhost', () => {
+	console.log('app is listening');
 });
 
-app.get("/", (req, res) => {
-  res.end("get method was fired");
+app.get('/', (req, res) => {
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	fs.readFile('./public/index.html', function(err, content) {
+		let contentString = content.toString('utf8');
+		let newString = contentString.replace('{{ commit Feed }}', dataString);
+		console.log(newString);
+		res.write(newString);
+	});
+	//res.end('get method was fired');
 });
 
-
-
-app.get("/foo/:bar", (req, res) => {
-  let keys = Object.keys(req.params);
-  keys.forEach(key => {
-    res.write(req.params[key]);
-  });
-  res.end("foo bar");
+app.get('/foo/:bar', (req, res) => {
+	let keys = Object.keys(req.params);
+	keys.forEach(key => {
+		res.write(req.params[key]);
+	});
+	res.end('foo bar');
 });
 
-app.post("/post/:form", (req, res) => {
-  let body = '';
-  /*
+app.post('/post/:form', (req, res) => {
+	let body = '';
+	/*
   req.on('data', (data) => {
     console.log('heres the data in req.on: ' + data);
     body += data
@@ -34,7 +46,7 @@ app.post("/post/:form", (req, res) => {
   })
   => moved router.js for router.handler
   */
-  //req.on(end... replaces res.end("form posted \n");
+	//req.on(end... replaces res.end("form posted \n");
 });
 
 //to test post run in terminal curl -X POST http://localhost:3000/post/sjdflksd
--- express/router.js
+++ express/router.js
@@ -1,54 +1,54 @@
-var url = require("url");
-var matchURLToPath = require("./pathmatcher");
-var parseParams = require("./parseParams");
+var url = require('url');
+var matchURLToPath = require('./pathmatcher');
+var parseParams = require('./parseParams');
 
 var Router = {};
 
-Router.methods = ["get", "post"];
+Router.methods = ['get', 'post'];
 
 Router.routes = {};
 
 Router.methods.forEach(method => {
-  Router.routes[method] = Router.routes[method] || {};
-  Router[method] = (path, callback) => {
-    Router.routes[method][path] = callback;
-  };
+	Router.routes[method] = Router.routes[method] || {};
+	Router[method] = (path, callback) => {
+		Router.routes[method][path] = callback;
+	};
 });
 
 Router.handle = (req, res) => {
-  let method = req.method.toLowerCase();
-  let purl = url.parse(req.url).pathname;
+	let method = req.method.toLowerCase();
+	let purl = url.parse(req.url).pathname;
 
-  let body = ''
-  req.on('data', (data) => {
-    console.log('heres the data in req.on: ' + data);
-    body += data
-  })
-  req.on('end', () => {
-    req.body = body
-    console.log('req.body is ' + req.body);
-    res.end('data')
-  })
+	let body = '';
+	req.on('data', data => {
+		console.log('heres the data in req.on: ' + data);
+		body += data;
+	});
+	// req.on('end', () => {
+	// 	req.body = body;
+	// 	console.log('req.body is ' + req.body);
+	// 	res.end('data');
+	//});
 
-  var callbackPath = purl;
+	var callbackPath = purl;
 
-  let patharray = Object.keys(Router.routes[method]);
-  //.post is still .get
-  patharray.forEach(path => {
-    if (matchURLToPath(purl, path)) {
-      callbackPath = path;
-    }
-  });
+	let patharray = Object.keys(Router.routes[method]);
+	//.post is still .get
+	patharray.forEach(path => {
+		if (matchURLToPath(purl, path)) {
+			callbackPath = path;
+		}
+	});
 
-  if (Router.routes[method][callbackPath]) {
-    let params = parseParams(callbackPath, purl);
-    req.params = params; //params = {...}
-    //if (method == 'post') { req.body = }
-    Router.routes[method][callbackPath](req, res);
-  } else {
-    res.statusCode = 404;
-    res.end("404 Not Found");
-  }
+	if (Router.routes[method][callbackPath]) {
+		let params = parseParams(callbackPath, purl);
+		req.params = params; //params = {...}
+		//if (method == 'post') { req.body = }
+		Router.routes[method][callbackPath](req, res);
+	} else {
+		res.statusCode = 404;
+		res.end('404 Not Found');
+	}
 };
 
 module.exports = Router;
