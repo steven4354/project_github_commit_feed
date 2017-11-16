c13c5cc98346e513f43b03e982a90d835d52c5ac
Steven Li <stevenli@Stevens-MacBook-Pro.local>
2017-11-16 08:42:46 -0800

    finished wrapper

---
+++ index.js
@@ -0,0 +1,3 @@
+var getcommitsObj = require('./wrapper.js')
+
+getcommitsObj('')
--- wrapper.js
+++ wrapper.js
@@ -1,15 +1,29 @@
 var Github = require('github');
 
-let GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN;
+var github = new Github({
+    // optional
+})
 
 github.authenticate({
 	type: 'token',
-	token: GITHUB_API_TOKEN
+	token: 'd553cea3a143ef191150caa9204a6524dca8483f'
 });
 
-console.log(
+//change the token after pulling
+
+var getcommitsObj(ownername, reponame){
 	github.repos.getCommits({
-		owner: 'fabpot',
-		repo: 'symfony'
+		owner: ownername,
+		repo: reponame
 	})
-);
+	.then(result => {
+		console.log('this is the result: ' + result)
+		return result
+	})
+	.catch(err => {
+		console.log('this is the err: ' + err)
+		return err
+	})
+}
+
+module.exports = getcommitsObj
